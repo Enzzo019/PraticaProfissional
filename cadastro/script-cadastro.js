d548edf336e0a85513854926df54662cd669bd6b
rafael-diogo <98774687+rafael-diogo@users.noreply.github.com>
2025-09-01 16:07:03 -0300

    Add files via upload

---
+++ cadastro/script-cadastro.js
@@ -0,0 +1,10 @@
+
+
+function dados() {
+    let name=document.getElementById('nome').value
+    let endereco=document.getElementById('endereco').value
+    let email=document.getElementById('email').value
+    let telefone=document.getElementById('telefone').value
+    return alert(telefone)
+}
+    
