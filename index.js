ef8c990d898d50f0a756747d9052971570b2e207
rafael-diogo <98774687+rafael-diogo@users.noreply.github.com>
2025-10-03 16:17:38 -0300

    api inseri get Update index.js
    para coloca nome no menu

--- index.js
+++ index.js
@@ -1,60 +1,41 @@
 require("dotenv").config();
 const porta = process.env.PORTA;
-const stringSQL = process.env.CONNECTION_STRING;
-const express = require('express');
+const stringSQL = process.env.CONECTION_STRING;
+const express = require("express");
 const app = express();
-const mssql = require('mssql');
+const mssql = require("mssql");
 const cors = require("cors");
+app.use(express.json())
 
 app.use(cors({
     origin:'*'
-}));
+}))
 
-app.use(express.json());
+app.use(express.json())
 
-// FUNÇÃO ACESSAR BANCO DE DADOS!
-async function connectBancoDados() {
-    try {
-        await mssql.connect(stringSQL)
-        console.log("Conexão ao Banco de Dados bem sucedida!")
-    } catch (erro) {
-        console.log("Erro na conexão ao Banco de Dados!",erro)
+async function conectaBD() {
+    try{
+        await mssql.connect(stringSQL);
+    }
+    catch (error){
+        console.log("erro na conexão com o BD",error)
     }
 }
-connectBancoDados();
+conectaBD() 
 
-app.get('/', (req, res) => {
-    res.send("Servidor funcionando!");
-});
-
-// GET
 app.get('/produtos',async (req,res)=>{
     const alunos = await mssql.query("SELECT * FROM daroca.produto")
     console.log(alunos)
     res.json(alunos);
 
 })
-// GET CATEGORIA
 app.get('/categorias',async (req,res)=>{
     const alunos = await mssql.query("SELECT * FROM daroca.categorias")
     console.log(alunos)
     res.json(alunos);
 
 })
-// GET COM PARAMETRO
-app.get('/produtos/:id', async (req,res) => {
-    const id = parseInt(req.params.id);
-    const resultados = await mssql.query('SELECT * FROM daroca.produtos WHERE ID=' + id);
-    if (resultados.lenght != 0) {
-        console.log(resultados.recordset)
-        res.json(resultados.recordset);
-    }
-    else {
-        res.status(404).json({ message: 'Produto não encontrado!'});
-    }
-})
 
-// POST
 app.post('/cadastro',(req,res)=>{
     try{
         
@@ -70,45 +51,19 @@ app.post('/cadastro',(req,res)=>{
         console.log("Erro na inserção de dados",erro)
     }
 })
-
-/// DELETE
-app.delete('/produtos/:id', async (req,res) => {
-    try {
-        const id = req.params.id;
-        const produto = await mssql.query(`DELETE daroca.produtos WHERE ID= ${id}`);
-        console.log("produto: " + produto.rowsAffected)
-        if (produto.rowsAffected == 1){
-            res.sendStatus(200).json({ message: 'Item excluido com sucesso!'});
-        }
-        else {
-            res.status(404).json({ message: 'Produto não encontrado!'});
-        }
-    }
-    catch (erro) {
-        res.status(404).json({ message: 'Erro na exclusão do produto!'})
-    }
-})
-
-// PATCH
-app.patch('/produtos/:id', async (req,res) => {
-    const id = req.params.id;
+app.get('/produtos/:nome',async (req,res)=>{
     const nome = req.params.nome;
-    const valor = req.params.valor;
-    const descricao = req.params.descricao;
-    try {
-        const sql = `UPDATE daroca.produtos SET nome='${nome}', valor='${valor}', descricao='${descricao}' WHERE ID=${id}`
-        const produtoAlterado =await mssql.query(sql);
-        console.log("produtoAlterado: " + produtoAlterado.rowsAffected)
-        res.sendStatus(200);
-    }
-    catch (erro) {
-        res.status(400).json({ error: `Erro ao alterar produto ${nome}, ${valor}, ${descricao}`});
-    }
+    const nome_produto = await mssql.query(`SELECT * FROM daroca.produto WHERE nome='${nome}'`)
+    res.json(nome_produto);
+})
+app.get('/cadastro/:email',async (req,res)=>{
+        const login_email = req.params.email;
+        const nome_produto = await mssql.query(`SELECT * FROM daroca.Clientes WHERE email='${login_email}'`)
+        res.json(nome_produto);
+
 })
 
-app.use("/produtos", (re,res) => res.json({ message: 'Servidor em execução!'}));
 
-app.listen(porta, () => console.log(`API rodando na porta ${porta}`));
-
-//// FALTAM OS MÉTODOS POST, PATCH, DELETE E GET COM PARAMETRO!!!!!!!!!!!!!!!
-
+app.use('/',(req,res) => res.json({mensagem: 'servidor em execusão'}) ) 
+    
+app.listen(porta, () => console.log("API funcionando!"))
