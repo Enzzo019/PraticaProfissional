a56539c8c3856951bcd800cfa2b93ace7d2bc259
Enzzo Ferreira <enzzoferreira69@gmail.com>
2025-10-12 18:07:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,38 @@
 require("dotenv").config();
-const porta = process.env.PORTA;
-const stringSQL = process.env.CONECTION_STRING;
 const express = require("express");
-const app = express();
-const mssql = require("mssql");
 const cors = require("cors");
-app.use(express.json())
+const mssql = require("mssql");
+const app = express();
+const porta = process.env.PORTA;
+const stringSQL = process.env.CONNECTION_STRING;
 
-app.use(cors({
-    origin:'*'
-}))
 
-app.use(express.json())
+app.use(cors({ origin: "*" }));
+app.use(express.json());
 
 async function conectaBD() {
-    try{
-        await mssql.connect(stringSQL);
-    }
-    catch (error){
-        console.log("erro na conexão com o BD",error)
-    }
+  try {
+    await mssql.connect(stringSQL);
+    console.log("Conexão com o banco realizada!");
+  } catch (error) {
+    console.log("Erro na conexão com o BD:", error);
+  }
 }
-conectaBD() 
+conectaBD();
 
-app.get('/produtos',async (req,res)=>{
-    const alunos = await mssql.query("SELECT * FROM daroca.produto")
-    console.log(alunos)
-    res.json(alunos);
 
-})
-app.get('/categorias',async (req,res)=>{
-    const alunos = await mssql.query("SELECT * FROM daroca.categorias")
-    console.log(alunos)
-    res.json(alunos);
+const produtoRouter = require("./routes/ProdutoRoutes");
+app.use("/produtos", produtoRouter);
 
-})
+const clienteRouter = require("./routes/ClienteRoutes");
+app.use("/clientes", clienteRouter);
 
-app.post('/cadastro',async(req,res)=>{
-    try{
-        
-        const nome=req.body.nome;
-        const endereco =req.body.endereco;
-        const email=req.body.email;
-        const telefone=req.body.telefone;
-        const senha=req.body.senha;
-        await mssql.query(`INSERT INTO daroca.Clientes(nome,endereco,email,telefone,senha) VALUES ('${nome}','${endereco}','${email}','${telefone}','${senha}')`)
-        res.status(201).json({ "sucesso": true, "mensagem": "dados inseridos com sucesso", "email" : email })
-    }
-    catch (erro) {
-        console.log("Erro na inserção de dados",erro)
-    }
-})
-app.get('/produtos/:nome',async (req,res)=>{
-    const nome = req.params.nome;
-    const nome_produto = await mssql.query(`SELECT * FROM daroca.produto WHERE nome='${nome}'`)
-    res.json(nome_produto);
-})
-app.get('/cadastro/:email',async (req,res)=>{
-        const login_email = req.params.email;
-        const nome_produto = await mssql.query(`SELECT * FROM daroca.Clientes WHERE email='${login_email}'`)
-        res.json(nome_produto);
+const categoriaRouter = require("./routes/CategoriaRoutes");
+app.use("/categorias", categoriaRouter);
 
-})
-app.get('/prod/:id',async (req,res)=>{
-    const id = req.params.id;
-    const nome_produto = await mssql.query(`SELECT * FROM daroca.produto WHERE id='${id}'`)
-    res.json(nome_produto);
-})
 
-app.use('/',(req,res) => res.json({mensagem: 'servidor em execusão'}) ) 
-    
-app.listen(porta, () => console.log("API funcionando!"))
+app.use("/", (req, res) =>
+  res.json({ mensagem: "Servidor em execução, API funcionando!" })
+);
+
+app.listen(porta, () => console.log(`API rodando na porta ${porta}`));
