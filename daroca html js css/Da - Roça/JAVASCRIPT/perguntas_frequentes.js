3e67bfeb8d7d48201127a85d5333b4237381cb4a
rafael-diogo <98774687+rafael-diogo@users.noreply.github.com>
2025-11-25 14:06:05 -0300

    Delete daroca html js css directory

--- daroca html js css/Da - Roça/JAVASCRIPT/perguntas_frequentes.js
+++
@@ -1,8 +0,0 @@
-const faqItems = document.querySelectorAll(".pergunta-item");
-
-faqItems.forEach(item => {
-    const question = item.querySelector(".pergunta-question");
-    question.addEventListener("click", () => {
-        item.classList.toggle("active");
-    });
-});
